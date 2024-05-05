be7b3c589f4f9e457f5974fb656d0dd8d2855d4d
wramos98 <wilfredo.ramos@tecsup.edu.pe>
2024-05-05 14:10:28 -0500

    Ultima entrega PC01

--- app/src/main/java/com/example/pc_wilfredoramos/MainActivity.kt
+++ app/src/main/java/com/example/pc_wilfredoramos/MainActivity.kt
@@ -9,12 +9,21 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        btnCalcular.setOnClickListener {
+            val promedio = calcularPromedio()
+            val intent = Intent(this, ResultadoActivity::class.java)
+            intent.putExtra("PROMEDIO", promedio)
+            startActivity(intent)
         }
     }
+
+    private fun calcularPromedio(): Double {
+        val n1 = nota1.text.toString().toDouble()
+        val n2 = nota2.text.toString().toDouble()
+        val n3 = nota3.text.toString().toDouble()
+        return (n1 + n2 + n3) / 3
+    }
 }
+
